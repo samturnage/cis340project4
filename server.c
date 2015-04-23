cfa47bbb8710fcbf05da7d861e9d381e604ecf0e
DOWU10 <daniellear18@yahoo.com>
2015-04-23 15:32:21 -0400

    Update server.c
    this is what I was thinking for the server side, since we have to create the socket, bind it try to open the imagefile.img, I was thinking could point to the char array, hostname[] that you defined on the client side, and then we can read into the buffer, and send data to the client. I read in the homework that upon successful connection to the client a handler similar to an fd, is sent and needs to be used as a reference, so I put in some of the methods  I think might work, if I am thinking correctly as how this is supposed to go.

--- server.c
+++ server.c
@@ -3,17 +3,55 @@
 #include <stdio.h>
 int main( int argc, const char* argv[] )
 {
-    	  const char* floppy = argv[1];
-	  printf( "\nFloppy %s opened...\n",floppy);
-	  //fmount
-	  
-	  //loop waiting for input
-	  while(0)
-	  {
-	      //if input is recieved, return the designated sector info in UDP
-	  
-	  }
-	  
+    	 // const char* floppy = argv[1];
+	 //printf( "\nFloppy %s opened...\n",floppy);
+	
+          //fmount
+    
+    
+    int cont, socket_fd, fd, fsize, ca;
+    int buffsize = 1024;
+    char *hostname[1024];
+    char *buffer = malloc(sizeof(buffsize)); //buffer to read imagefile.img into
+    struct sockaddr_in s_in, from;
+    
+    
+    socket_fd = socket(AF_INET, SOCK_DGRAM, 0);  //create socket
+    if(socket_fd < 0){
+        perror("socket not created"); //socket error
+        exit(1);
+    }
+    bzero((char *)&s_in, sizeof(s_in));
+    
+    s_in.sin_family = (short)AF_INET;            //define the family, address and port ...I think we can use 5000? it was between 5000- 6000 something
+    s_in.sin_addr.s_addr = hton(INADDR_ANY);
+    s_in.sin_port = htons ((u_short)0x5000);
+    
+    if(bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in)) < 0){  //bind socket
+        perror("bind failed");
+        exit(1);
+    }
+    for(;;){
+        fsize = sizeof(from);
+        ca = recvfrom(socket_fd, hostname, 1024, 0, (struct sockaddr *)&from,&fsize);  //receive data from UDP datagram
+        if(ca < 0){
+            perror("recvfrom fail");
+        }
+        
+        if((fd = open(hostname, O_RDONLY)) < 0){ //open the imagefile (fmount)
+            perror("file open failed");
+        }
+        
+        while((cont = read(fd, buffer, sizeof(buffer)))>0) {  // while loop reading from the sectors possibly need to manipulate this to read from individual sectors
+            sleep(1);
+            sendto(socket_fd, hostname, ca, 0, (struct sockaddr *)&from,&fsize);  //trying to send back to client 
+            
+        }
+        
+    }
+        //fumount
+        printf("request complete");
+        close(socket_fd);
 	  //fumount
 	  return 0;
 }
