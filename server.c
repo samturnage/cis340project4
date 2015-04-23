048057ff19b81f2378d1e1041e269e30c4743fc3
Samuel Turnage <samturnage4@yahoo.com>
2015-04-23 09:51:27 -0400

    Create server.c

---
+++ server.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+int main( int argc, const char* argv[] )
+{
+    char[] floppy = argv[1];
+	  printf( "\nFloppy %s opened...\n",floppy);
+	  //fmount
+	  
+	  //loop waiting for input
+	  while(0)
+	  {
+	      //if input is recieved, return the designated sector info in UDP
+	  
+	  }
+	  
+	  //fumount
+	  return 0;
+}
