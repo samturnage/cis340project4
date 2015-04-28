2f72819719eadf0acf6df282bc10457c52b8b4cf
DOWU10 <daniellear18@yahoo.com>
2015-04-28 16:43:50 -0400

    Update server.c

--- server.c
+++ server.c
@@ -1,6 +1,18 @@
 
 
-#include <stdio.h>
+#include        <stdio.h>
+#include	<signal.h>
+#include	<errno.h>
+#include	<strings.h>
+#include 	<stdlib.h>
+#include	<stdio.h>
+#include	<sys/types.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<netdb.h>
+#include        <arpa/inet.h>
+
+
 int main( int argc, const char* argv[] )
 {
     	 // const char* floppy = argv[1];
