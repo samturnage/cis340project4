12b2b23b4305756ecf6f976a43a45de0bb7b3f68
Samuel Turnage <samturnage4@yahoo.com>
2015-05-02 01:30:07 -0400

    Update client.c

--- client.c
+++ client.c
@@ -94,7 +94,7 @@ void help()
 void structure()   //int fdif we can pass in the fd it will be so much easier
 {
     unsigned char buff[512];
-    char *message = "structure"
+    char *message = "structure";
     if (sendto(soket_fd, message, strlen(message) , 0 , (struct sockaddr *) &address, sizeof(address))==-1)
     {
             die("sendto()");
