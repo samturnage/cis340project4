d7a5dc32611e42e3dccc183c1098f2d7326624ef
Samuel Turnage <samturnage4@yahoo.com>
2015-05-03 17:04:58 -0400

    Update server.c

--- server.c
+++ server.c
@@ -95,7 +95,7 @@ int main()
         //printf("Received: %s\n", temp->message);
         
         //printf("\nReceived packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        //printf("Command: %s\n" , message->);
+        printf("Command: %s\n" , message->command);
         printf("Argument: %u\n" , message->argument);
         //printf("Data: %s\n" , message->data);
         //now reply the client with the same data
