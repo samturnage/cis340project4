8cdcc4edd63f4cc0c5f4e6008ab24ab3fea89dcf
Samuel Turnage <samturnage4@yahoo.com>
2015-05-04 00:05:27 -0400

    Update server.c

--- server.c
+++ server.c
@@ -79,7 +79,7 @@ int main()
             lseek(floppy_fd, 0, SEEK_SET );
             char flop[512];
             read(floppy_fd, flop, sizeof(flop));
-            printf("\nData: %*s" , flop,512);
+            printf("\nData: %*s",512 , flop);
             if (sendto(socket_fd, message, sizeof(*message), 0, (struct sockaddr*) &si_other, slen) == -1)
             {
             	die("\nsendto()");
